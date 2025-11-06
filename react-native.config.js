272717ff747386399f1525829d8363dc1c74f981
Gabor Pal Korom <gabor.p.korom@migeran.com>
2025-11-06 10:39:03 +0100

    Enable new arch for Android example app and fix path to generated code.

--- react-native.config.js
+++ react-native.config.js
@@ -4,7 +4,7 @@ module.exports = {
       android: {
         // Subdirectory paths in Android-autolinking.cmake are generated based on these:
         // Paths given are relative to the ./android folder:
-        cmakeListsPath: "./src/generated/jni/CMakeLists.txt",
+        cmakeListsPath: "./build/generated/source/codegen/jni/CMakeLists.txt",
       },
     },
   },
